dd5288d68857468aed98f1456bf6d216c1c8726b
CommanderNate <natecowie@gmail.com>
2019-11-17 12:32:57 -0500

    Finished lab 6

--- app/src/main/java/com/example/androidlabs/ProfileActivity.java
+++ app/src/main/java/com/example/androidlabs/ProfileActivity.java
@@ -5,9 +5,7 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
@@ -18,14 +16,12 @@ public class ProfileActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     public static final String KEY_Email = "KEY_Email";
     private ImageButton mImageButton;
-    public static final String ACTIVITY_NAME = "PROFILE_ACTIVITY";
-    private Button Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_linear);
-        Log.e(ACTIVITY_NAME, "In function: onCreate");
+
         Intent it;
         it = getIntent();
         it.getStringExtra(KEY_Email);
@@ -36,66 +32,30 @@ public class ProfileActivity extends AppCompatActivity {
         mImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dispatchTakePictureIntent();
+         dispatchTakePictureIntent();
             }
         });
-
-        Button = findViewById(R.id.button);
-        Button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent it = new Intent(ProfileActivity.this, ChatRoomActivity.class);
-                startActivity(it);
-            }
-        });
-
     }
 
     private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
 
     }
 
-
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
             mImageButton.setImageBitmap(imageBitmap);
-
         }
-        Log.e(ACTIVITY_NAME, "In function: onActivityResult");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.e(ACTIVITY_NAME, "In function: onDestroy");
     }
-
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.e(ACTIVITY_NAME, "In function: onStop");
-    }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.e(ACTIVITY_NAME, "In function: onResume");
-    }
-
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.e(ACTIVITY_NAME, "In function: onStart");
-    }
-
-
 }
